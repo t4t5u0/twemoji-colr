8071323f527b6b2c24ddaf40214cdc3071763938
t4t5u0 <ymmtryk0902@gmail.com>
2023-03-04 23:49:28 +0900

    Update: skip especially gryph

--- fixDirection.py
+++ fixDirection.py
@@ -6,6 +6,7 @@ fontfile = sys.argv[1]
 f = fontforge.open(fontfile)
 
 for glyph in f.glyphs():
-    glyph.correctDirection()
+    if glyph.encoding not in [0x23, 0x2a, 0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39]:
+        glyph.correctDirection()
 
 f.generate(fontfile)
